b10dcad3975466d4bcc27e1783cf81068de01d8c
Chris Morbitzer <chris.morbitzer@gmail.com>
2020-04-01 18:58:42 -0500

    Add title

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 // import Header from "./header";
 import Footer from "./footer";
 import styles from "./layout.module.scss";
 
 export default ({ children }) => (
   <div className={styles.container}>
+    <Helmet>
+      <title>Chris Morbitzer</title>
+    </Helmet>
+
     {/*<Header/>*/}
 
     <main>
